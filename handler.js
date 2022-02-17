73c78b12d999a6f9a7979f57d1966d32333b392c
Fredowisk <fredowisk@github.com>
2022-02-16 22:28:53 -0300

    Getting usage and usage plans

--- handler.js
+++ handler.js
@@ -1,15 +1,45 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async (event) => {
+const AWS = require("aws-sdk");
+const apiGateway = new AWS.APIGateway();
+const moment = require("moment");
+
+const hello = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: "Hello World!",
   };
+};
+
+const usage = async (event) => {
+  const { from, to, usagePlanId, keyId } = event.queryStringParameters;
+
+  const usage = await apiGateway
+    .getUsage({
+      endDate: moment(to).format("YYYY-MM-DD"),
+      startDate: moment(to).format("YYYY-MM-DD"),
+      usagePlanId,
+      keyId,
+    })
+    .promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(usage, null, 2),
+  };
+};
+
+const usagePlans = async (event) => {
+  const result = await apiGateway.getUsagePlans().promise();
+  console.log("Usage Plans", result);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result, null, 2),
+  };
+};
+
+module.exports = {
+  hello,
+  usage,
+  usagePlans,
 };
--- requests/request.sh
+++ requests/request.sh
@@ -1,15 +1,30 @@
-# HOST=http://localhost:3000
-HOST='Your host'
-# APIKEY="d41d8cd98f00b204e9800998ecf8427e"
-APIKEY="Your api key"
+HOST=http://localhost:3000
+# HOST='Your host'
+APIKEY="d41d8cd98f00b204e9800998ecf8427e"
+# APIKEY="Your api key"
 curl --silent \
     -H "x-api-key: $APIKEY" \
     $HOST/dev/hello
 
-echo "Press <Ctrl + C> to exit."
-while :
-do
-  curl --silent \
-    -H "x-api-key: $APIKEY" \
-    $HOST/dev/hello
-done
+# echo "Press <Ctrl + C> to exit."
+# while :
+# do
+#   curl --silent \
+#     -H "x-api-key: $APIKEY" \
+#     $HOST/dev/hello
+# done
+
+curl --silent \
+    $HOST/dev/getUsagePlans | tee getUsagePlans.log
+
+#from get usage plans
+#0b8tbe is for free plans
+USAGE_PLAN_ID="0b8tbe"
+KEYID='Your key id'
+APIKEY="Your api Key"
+FROM="2022-02-16"
+TO="2022-02-17"
+
+curl --silent \
+    "$HOST/dev/getUsage?keyId=$KEYID&usagePlanId=$USAGE_PLAN_ID&from=$FROM&to=$TO" \
+    | tee usage.log

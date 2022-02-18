f7976b63d816f12ef3916b236c70471bf864b426
Fredowisk <fredowisk@github.com>
2022-02-17 21:54:42 -0300

    Creating api keys

--- requests/request.sh
+++ requests/request.sh
@@ -1,5 +1,5 @@
 HOST=http://localhost:3000
-# HOST='Your host'
+
 APIKEY="d41d8cd98f00b204e9800998ecf8427e"
 # APIKEY="Your api key"
 curl --silent \
@@ -20,11 +20,17 @@ curl --silent \
 #from get usage plans
 #0b8tbe is for free plans
 USAGE_PLAN_ID="0b8tbe"
-KEYID='Your key id'
-APIKEY="Your api Key"
+KEYID='your key id'
+APIKEY="your api key"
 FROM="2022-02-16"
-TO="2022-02-17"
+TO="2022-02-18"
 
 curl --silent \
     "$HOST/dev/getUsage?keyId=$KEYID&usagePlanId=$USAGE_PLAN_ID&from=$FROM&to=$TO" \
     | tee usage.log
+
+CUSTOMER_NAME=customer@test.com
+
+curl --silent \
+    "$HOST/dev/addKey?name=$CUSTOMER_NAME&usagePlanId=$USAGE_PLAN_ID" \
+    | tee addkey.log
